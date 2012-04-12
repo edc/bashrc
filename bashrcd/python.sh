771f27f11ab2ee4cd6671fb012717006444d1d51
Eddie C <cao.yiqun@gmail.com>
2012-04-11 21:02:47 -0700

    pip download cache

--- bashrcd/python.sh
+++ bashrcd/python.sh
@@ -2,3 +2,5 @@ export WORKON_HOME=$HOME/devel/_envs
 source /usr/local/bin/virtualenvwrapper.sh
 alias serv='python manage.py runserver'
 export PYTHONDONTWRITEBYTECODE=1
+export PIP_DOWNLOAD_CACHE=$HOME/Library/Caches/pip-downloads
+
